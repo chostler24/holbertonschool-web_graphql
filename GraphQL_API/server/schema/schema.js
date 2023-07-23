73e1611dfd970568fddea34a656fa7554288e441
chostler24 <connor.hostler@gmail.com>
2023-07-23 17:36:35 -0500

    updated schema file

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,6 +1,7 @@
 // schema file for GraphQL
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
 
+// TaskType definition
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: {
@@ -11,6 +12,7 @@ const TaskType = new GraphQLObjectType({
   },
 });
 
+// RootQuery definition
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -20,21 +22,19 @@ const RootQueryType = new GraphQLObjectType({
         id: { type: GraphQLString },
       },
       resolve(parent, args) {
-        // Add your code here to fetch data from the database based on the provided id
-        // For simplicity, let's assume we have a tasks array with dummy data
-        const tasks = [
-          { id: '1', title: 'Task 1', weight: 1, description: 'Description of Task 1' },
-          { id: '2', title: 'Task 2', weight: 2, description: 'Description of Task 2' },
-          { id: '3', title: 'Task 3', weight: 3, description: 'Description of Task 3' },
-        ];
-        return tasks.find(task => task.id === args.id);
+        const task = {
+          id: args.id,
+          title: 'Sample Task',
+          weight: 5,
+          description: 'This is a sample task',
+        };
+        return task;
       },
     },
   },
 });
 
-const schema = new GraphQLSchema({
+// Export the GraphQLSchema with RootQuery
+module.exports = new GraphQLSchema({
   query: RootQueryType,
 });
-
-module.exports = schema;
