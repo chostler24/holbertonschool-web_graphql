676c770a4a2b2b54f3960d87ed39aa8e007360f1
chostler24 <connor.hostler@gmail.com>
2023-07-24 16:09:04 -0500

    app changes, filename change and 2 file creation

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,12 +1,16 @@
 // initializes express server through /graphql route
 
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
 }));
-app.listen(4000,()=>{
+
+app.listen(4000, () => {
   console.log('now listening for request on port 4000');
 });
