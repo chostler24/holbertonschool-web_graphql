d2e3e2144c5b7472408c8a1d0e37ac7f1108736d
chostler24 <connor.hostler@gmail.com>
2023-07-25 11:41:42 -0500

    task 9 and gitignore files

--- GraphQL_API/client/src/App.js
+++ GraphQL_API/client/src/App.js
@@ -1,29 +1,30 @@
-/*import ApolloClient from 'apollo-boost';
-/*
-...
-*/
+// setup for Apollo Client
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 // components
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import AddProject from './components/AddProject';
+
 // apollo client setup
-/*
-...
-*/
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
 function App() {
-  return ( <
-    div id = "main" >
-    <
-    div id = "bg" > < /div> <
-    h1 > Holberton school tasks < /h1> <
-    TaskList / >
-    <
-    AddProject / >
-    <
-    AddTask / >
-    <
-    /div>
+  return (
+    <ApolloProvider client={client}>
+      <div id="main">
+        <div id="bg"></div>
+        <h1>Holberton school tasks</h1>
+        <TaskList />
+        <AddProject />
+        <AddTask />
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
