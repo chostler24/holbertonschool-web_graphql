4e84504768b2e233ba2f2ce296f988264f9372a8
chostler24 <connor.hostler@gmail.com>
2023-07-25 12:07:35 -0500

    task 10

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -4,8 +4,11 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
@@ -16,7 +19,7 @@ app.listen(4000, () => {
   console.log('now listening for request on port 4000');
 });
 
-const connectionString = 'mongodb+srv://Sarah:YmAewJcKBagm2ky0@cluster0.gvom3z3.mongodb.net/?retryWrites=true&w=majority';
+const connectionString = 'mongodb+srv://Connor:<password>@cluster0.z3tvuzo.mongodb.net/';
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
